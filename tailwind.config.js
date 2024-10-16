f655fe08e3995cb08e64d944b1496d614d079f03
Siripoom Limsoontorn <65209010011@thonburi.ac.th>
2024-10-16 15:10:57 +0700

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        body: '#fff',
+        'black-clr': '#000',
+        'white-clr': '#fff',
+        'p1-clr': '#2ab939',
+        'p2-clr': '#fdd61f',
+        'p900-clr': '#1f4e3d',
+        'pure900-clr': '#225744',
+        'p800-clr': '#6d756d',
+        'p700-clr': '#191919',
+        'p200-clr': '#e9efe5',
+        'p100-clr': '#eff3ed',
+        'box-clr': '#2A5747',
+      },
+      boxShadow: {
+        'custom-shadow': 'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px',
+      },
+    },
   },
   plugins: [],
 }
